fc45075022f8c4c78ac79d4b29d5977783851823
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-08-20 15:50:32 +0900

    result

--- abc124/D.py
+++ abc124/D.py
@@ -1,30 +1,45 @@
+import sys
+
 def main():
     N,K = [int(i) for i in input().split()]
     S = input()
     count = list()
+    number = list()
     c = 0
+    i = 0
     count.append(c)
-    i = 0
     while i < N:
         now = S[i]
-        while(S[i]==now):
+        if now == '1':
+            number.append('1')
+        else:
+            number.append('0')
+        while(S[i] == now):
             c += 1
-            i += 1
-            if i == N :
-                break
-        count.append(c)
-    max_c = 0
-    i = 0
-    while i < N:
-        now = S[i]
-        if now == '0' and i + 2 * K < N:
-            max_c = max(max_c,count[i + 2 * K] - count[i])
-        elif now == '1' and i + 2 * K + 1 < N:
-            max_c = max(max_c,count[i + (2 * K + 1)] - count[i])
-        while(S[i]==now):
             i += 1
             if i == N:
                 break
-    print(max_c)
+        count.append(c)
+    c = 0
+    for i in range(len(number)):
+        if number[i] == '0':
+            c += 1
+    if c <= K:
+        print(len(S))
+        sys.exit()
+    else:
+        max_c = 0
+        i = 0
+        while i < len(number):
+            now = number[i]
+            if now == '0' and i + 2 * K < len(count):
+                max_c = max(max_c,count[i + 2 * K] - count[i])
+            elif now == '1' and i + 2 * K + 1 < len(count):
+                max_c = max(max_c,count[i + 2 * K + 1] - count[i])
+            else:
+                max_c = max(max_c,count[-1] - count[i])
+            i += 1
+        print(max_c)
+
 if __name__ == "__main__":
     main()
