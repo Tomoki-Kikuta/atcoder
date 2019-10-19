11af11b988b6ba033c9d823c7844faef3952351f
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-19 23:16:06 +0900

    result

--- abc143/E.py
+++ abc143/E.py
@@ -2,41 +2,46 @@ INF = 10000000000
 import math
 def main():
     N, M, L = map(int, input().split())
+
     d = [[INF for i in range(N)] for j in range(N)]
-    pre = [[i for i in range(N)] for j in range(N)]
+    ans_d = [[INF for i in range(N)] for j in range(N)]
+    for i in range(N):
+        d[i][i] = 0
+        ans_d[i][i] = 0
+
     for i in range(M):
         a, b, c = map(int, input().split())
         a -= 1
-        b -= 1
-        if c > L:
-            continue 
+        b -= 1 
         d[a][b] = c
         d[b][a] = c
+
     for k in range(N):
         for i in range(N):
             for j in range(N):
-                if d[i][j] > d[i][k] + d[k][j]:
-                    pre[i][j] = k
-                    d[i][j] = d[i][k] + d[k][j]
-    #print(pre)
+                    d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                continue
+            if d[i][j] <= L:
+                ans_d[i][j] = 1
+
+    for k in range(N):
+        for i in range(N):
+            for j in range(N):
+                    ans_d[i][j] = min(ans_d[i][j], ans_d[i][k] + ans_d[k][j])
+
     Q = int(input())
     for i in range(Q):
-        count = 0
         s, t = map(int, input().split())
         s -= 1
         t -= 1
         if d[s][t] == INF:
             print(-1)
         else:
-            k = t
-            distance = 0
-            while pre[s][k] != s:
-                k = pre[s][k]
-                distance += d[t][k]
-                if distance >= L:
-                    distance -= L
-                    count += 1
-            print(count)
+            print(ans_d[s][t] - 1)
 
 
 
