7120096a2a6b87c013dc803ede8eadc676b03d84
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-10-20 15:45:43 +0900

    result

--- abc143/E.py
+++ abc143/E.py
@@ -1,50 +1,50 @@
-INF = 10000000000
-import math
+"""ライブラリ使わないと間に合わなかった"""
+from scipy.sparse.csgraph import csgraph_from_dense, floyd_warshall
+INF = 1001001001
+
+
 def main():
     N, M, L = map(int, input().split())
 
-    d = [[INF for i in range(N)] for j in range(N)]
-    ans_d = [[INF for i in range(N)] for j in range(N)]
-    for i in range(N):
-        d[i][i] = 0
-        ans_d[i][i] = 0
-
+    fuel = [[INF] * N for _ in range(N)]
     for i in range(M):
-        a, b, c = map(int, input().split())
-        a -= 1
-        b -= 1 
-        if c > L:
-            continue
-        d[a][b] = c
-        d[b][a] = c
+        A, B, C = map(int, input().split())
+        A -= 1
+        B -= 1
+        fuel[A][B] = C
+        fuel[B][A] = C
+    for i in range(N):
+        fuel[i][i] = 0
 
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                    d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+    fuel = floyd_warshall(csgraph_from_dense(fuel))
+    # for k in range(N):
+    #     for i in range(N):
+    #         for j in range(N):
+    #             fuel[i][j] = min(fuel[i][j], fuel[i][k] + fuel[k][j])
 
+    dist = [[INF] * N for _ in range(N)]
     for i in range(N):
         for j in range(N):
             if i == j:
-                continue
-            if d[i][j] <= L:
-                ans_d[i][j] = 1
+                dist[i][j] = 0
+            elif fuel[i][j] <= L:
+                dist[i][j] = 1
 
-    for k in range(N):
-        for i in range(N):
-            for j in range(N):
-                    ans_d[i][j] = min(ans_d[i][j], ans_d[i][k] + ans_d[k][j])
+    dist = floyd_warshall(csgraph_from_dense(dist))
+    # for k in range(N):
+    #     for i in range(N):
+    #         for j in range(N):
+    #             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
     Q = int(input())
     for i in range(Q):
         s, t = map(int, input().split())
         s -= 1
         t -= 1
-        if d[s][t] == INF:
+        if dist[s][t] == INF:
             print(-1)
         else:
-            print(ans_d[s][t] - 1)
-
+            print(int(dist[s][t]) - 1)
 
 
 if __name__ == "__main__":
