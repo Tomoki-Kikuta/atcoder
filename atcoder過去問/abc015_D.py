5eec7800b9ed8204a83e04de798b8b923b72d03f
アルゴリズム <tomoki0819@akane.waseda.jp>
2019-11-03 16:54:54 +0900

    MLE

--- atcoder過去問/abc015_D.py
+++ atcoder過去問/abc015_D.py
@@ -1,26 +1,25 @@
-def make_dp(N, W, K):
+def make_dp(W, K):
     dp = []
-    for i in range(N + 1):
+    for j in range(W + 1):
         dp.append([])
-        for j in range(W + 1):
-            dp[i].append([])
-            for k in range(K + 1):
-                dp[i][j].append(0)
+        for k in range(K + 1):
+            dp[j].append(0)
     return dp
 
 
 def cal_ans(N, W, K, dp, width, importance):
     for i in range(1, N + 1):
-        for j in range(1, W + 1):
+        # i - 1番目まで使った時
+        for j in range(W, -1, -1):
+            # 幅jを作る時
             for k in range(1, K + 1):
+                # k枚使った時
                 if width[i - 1] <= j:
-                    dp[i][j][k] = max(dp[i - 1][j][k],
-                                      dp[i - 1][j - width[i - 1]][k - 1]
-                                      + importance[i - 1])
-                else:
-                    dp[i][j][k] = dp[i - 1][j][k]
+                    dp[j][k] = max(dp[j][k],
+                                   dp[j - width[i - 1]][k - 1]
+                                   + importance[i - 1])
 
-    return dp[N][W][K]
+    return dp[W][K]
 
 
 def main():
@@ -33,7 +32,7 @@ def main():
         width.append(a)
         importance.append(b)
 
-    dp = make_dp(N, W, K)
+    dp = make_dp(W, K)
     ans = cal_ans(N, W, K, dp, width, importance)
     print(ans)
 
